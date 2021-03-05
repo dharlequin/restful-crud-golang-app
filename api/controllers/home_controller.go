5b7e8e93e78dda33141cd1b102c5ebb7e984e3ef
Anton Pavlov <dharlequin@gmail.com>
2021-03-05 10:47:17 +0300

    - removed all traces of hardcoded .env file;

--- api/controllers/home_controller.go
+++ api/controllers/home_controller.go
@@ -8,5 +8,5 @@ import (
 
 //Home welcomes us in our API
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
-	responses.JSON(w, http.StatusOK, "Welcome To This Awesome User API, version 1.0.0-4")
+	responses.JSON(w, http.StatusOK, "Welcome To This Awesome User API, version 1.0.0-5")
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,5 @@ require (
 	github.com/badoux/checkmail v1.2.1
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
-	github.com/joho/godotenv v1.3.0
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
 )
